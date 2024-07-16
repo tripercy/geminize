a23af4d0cbb23293e45faa4e6ca2e482964cee1b
QuocTran3112 <hakuhatsuma1504@gmail.com>
2024-07-16 10:02:21 +0700

    Create idle animation in 4 direction

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,26 +6,41 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     private Vector3 dest;
+    private Animator animator;
     void Start()
     {
         dest = transform.position;
     }
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, dest, moveSpeed * Time.deltaTime);
-        
-        if (Vector3.Distance(transform.position, dest) < Mathf.Epsilon) {
+
+        if (Vector3.Distance(transform.position, dest) < Mathf.Epsilon)
+        {
             Vector2 input;
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            if (input.x != 0) {
-                dest += new Vector3(input.x, 0, 0);
-            } else if (input.y != 0) {
-                dest += new Vector3(0, input.y, 0);
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                if (input.x != 0)
+                {
+                    dest += new Vector3(input.x, 0, 0);
+                }
+                else if (input.y != 0)
+                {
+                    dest += new Vector3(0, input.y, 0);
+                }
             }
         }
     }
